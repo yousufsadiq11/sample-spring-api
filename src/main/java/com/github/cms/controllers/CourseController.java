b5188839fc4da86f6882e9e596998c0655ff1616
y0m00m7 <yousuf.mohammed0@walmart.com>
2020-03-08 23:33:42 -0500

    Integrated with spring cloud server

--- src/main/java/com/github/cms/controllers/CourseController.java
+++ src/main/java/com/github/cms/controllers/CourseController.java
@@ -4,6 +4,7 @@ import com.github.cms.models.Course;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,17 +22,15 @@ public class CourseController {
 
     @GetMapping("")
     @ApiOperation(value = "Get Courses List", response = ArrayList.class)
-    private ResponseEntity<List<Course>> getCourses(String studentId, String collegeId){
+    private ResponseEntity<List<Course>> getCourses(String studentId, String collegeId, @Value("${configTestValue}") String valueFromConfigServer){
         ArrayList<Course> courseList = new ArrayList<>();
         try{
             log.info("Calling courses Service:: Get Courses");
-            Course machineLearning = new Course(1,"Machine Learning");
-            Course artificialIntelligence = new Course(1,"Artificial Intelligence");
+            Course machineLearning = new Course(Integer.parseInt(valueFromConfigServer),"Machine Learning");
             courseList.add(machineLearning);
-            courseList.add(artificialIntelligence);
         }
         catch (Exception ex){
-            log.error("Error calling BinAudit Service::"+ex.getMessage());
+            log.error("Error calling Get Course Service::"+ex.getMessage());
         }
         return new ResponseEntity<>(courseList, HttpStatus.OK);
     }
